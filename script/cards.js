3c174088f9f8f217d88c540dc1b692171119eb2c
afkmaster <kuanwchen@gmail.com>
2025-07-17 10:53:51 -0700

    Update cards.js

--- script/cards.js
+++ script/cards.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   $('.cardInner a.cardStr').on('click', function() {
-    
     const body = document.querySelector('body');
-    const cardDetails = this.nextElementSibling;
+    const cardDetails = this.parentElement.querySelector('.cardDetailPageCol');
+    const bg = this.parentElement.querySelector('.detailBg');
+    
     body.classList.add('show-details');
     cardDetails.classList.add('active');
+    bg.classList.add('active');
   });
+
+  $('.cardInner .detailBg').on('click', function() {
+    const body = document.querySelector('body');
+    const cardDetails = this.parentElement.querySelector('.cardDetailPageCol');
+    
+    body.classList.remove('show-details');
+    cardDetails.classList.remove('active');
+    this.classList.remove('active');
+  }
 });
